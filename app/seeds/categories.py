806a01bea443eb09d8a173b4705a82db841c0626
CaSandraSmith <casresm3@gmail.com>
2023-06-02 15:18:07 -0700

    Changed pin category names

--- app/seeds/categories.py
+++ app/seeds/categories.py
@@ -4,17 +4,21 @@ from app.models import db, Category, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_categories():
-    category1 = Category(name="Modern")
+    category1 = Category(name="Streetware")
     db.session.add(category1)
-    category2 = Category(name="Cute")
+    category2 = Category(name="Formal Ware")
     db.session.add(category2)
     category3 = Category(name="Dark")
     db.session.add(category3)
     category4 = Category(name="Vintage")
     db.session.add(category4)
-    category5 = Category(name="Classic")
+    category5 = Category(name="Old Money")
     db.session.add(category5)
     db.session.commit()
+    category6 = Category(name="Athleisure")
+    db.session.add(category6)
+    db.session.commit()
+
 
 def undo_categories():
     if environment == "production":
