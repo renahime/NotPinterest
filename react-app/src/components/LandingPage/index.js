23baac36ef6c79f688bfd737797dca449463faa5
Michael Golshani <michaelgolshani@gmail.com>
2023-06-03 16:42:08 -0700

    created landing page. Log in and Sign up buttons need refining with CSS. Also need to add bottom portion of landing page

---
+++ react-app/src/components/LandingPage/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './LandingPage.css'
+
+function LandingPage() {
+  return (
+    <div className="container">
+      
+
+      <div className="h1">
+        <span>Get your next</span>
+        <br />
+        <span className="h1-blue-text">new look outfit</span>
+      </div>
+
+      <div className="images-main-page">
+        <img
+          src="https://i.pinimg.com/564x/5f/60/3f/5f603fba7b0ae07f66ce429553f3ac14.jpg"
+          // alt="Image 1"
+        />
+        <img
+          src="https://i.pinimg.com/564x/a4/b3/e0/a4b3e0e6ae852bca145fe32c40e093a2.jpg"
+          // alt="Image 2"
+        />
+        <img
+          src="https://i.pinimg.com/564x/63/3d/ee/633dee2ad3a5dc08925c911f7f25cb2d.jpg"
+          // alt="Image 3"
+        />
+        <img
+          src="https://i.pinimg.com/564x/c9/81/85/c98185782bd5aadd8606003b0da76696.jpg"
+          // alt="Image 4"
+        />
+        <img
+          src="https://i.pinimg.com/736x/6b/14/21/6b1421585dca717b2eee20b12757d9fe.jpg"
+          // alt="Image 5"
+        />
+        <img
+          src="https://i.pinimg.com/564x/5f/16/3f/5f163f2994d73a1f470adcaac95ef08f.jpg"
+          // alt="Image 6"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSNdvTkMY2clFD6tu6PSjaa3N0hzGLRhtKCH1Z2vyV6HTbB8QYlIzCAwozHEMU5SWcMl_Ldoml35ceMPUlkJSZ7g_wOWIeAjNrttc9bt_uQJ3nBCbAPC5Tz6eCe8Ly7zjnqzDU&usqp=CAc"
+          // alt="Image 7"
+        />
+      </div>
+
+
+    </div>
+  );
+}
+
+export default LandingPage;
