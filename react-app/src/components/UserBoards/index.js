895774a04529e01e7a814b267ebde08ebbce2074
CaSandraSmith <casresm3@gmail.com>
2023-06-05 22:56:58 -0700

    Added lock icon to private boards

--- react-app/src/components/UserBoards/index.js
+++ react-app/src/components/UserBoards/index.js
@@ -26,37 +26,42 @@ export default function UserBoards({userBoardsArr}) {
         <div className="profile-boards-all">
             {userBoardsArr.map(boards => (
                 <div>
-                    <div className="profile-board-images-wrapper">
-                        {
-                            boards.cover_image.length ? 
-                            <img className="profile-board-images1" src={boards.cover_image[0]} />
-                            : 
-                            boards.additional_images[0] ?
-                            <img className="profile-board-images1" src={boards.additional_images[0]} />
-                            :
-                            <img className="profile-board-images1" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
-                        }
-                        {
-                            boards.additional_images[0] ?
-                            <img className="profile-board-images1" src={boards.additional_images[0]} />
-                            :
-                            boards.additional_images[1] ? 
-                            <img className="profile-board-images2" src={boards.additional_images[1]} />
-                            :
-                            <img className="profile-board-images2" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
-                        }
-                        {
-                            boards.additional_images[0] ?
-                            <img className="profile-board-images1" src={boards.additional_images[0]} />
-                            :
-                            boards.additional_images[1] ? 
-                            <img className="profile-board-images2" src={boards.additional_images[1]} />
-                            :
-                            boards.additional_images[2] ? 
-                            <img className="profile-board-images3" src={boards.additional_images[2]} /> 
-                            :
-                            <img className="profile-board-images3" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
-                        }
+                    <div className="profile-board-pics">
+                        {boards.private ? <div className="profile-board-lock-icon-wrapper">
+                            <i class="fa-solid fa-lock"></i>
+                        </div> : null}
+                        <div className="profile-board-images-wrapper">
+                            {
+                                boards.cover_image.length ? 
+                                <img className="profile-board-images1" src={boards.cover_image[0]} />
+                                : 
+                                boards.additional_images[0] ?
+                                <img className="profile-board-images1" src={boards.additional_images[0]} />
+                                :
+                                <img className="profile-board-images1" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
+                            }
+                            {
+                                boards.additional_images[0] ?
+                                <img className="profile-board-images1" src={boards.additional_images[0]} />
+                                :
+                                boards.additional_images[1] ? 
+                                <img className="profile-board-images2" src={boards.additional_images[1]} />
+                                :
+                                <img className="profile-board-images2" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
+                            }
+                            {
+                                boards.additional_images[0] ?
+                                <img className="profile-board-images1" src={boards.additional_images[0]} />
+                                :
+                                boards.additional_images[1] ? 
+                                <img className="profile-board-images2" src={boards.additional_images[1]} />
+                                :
+                                boards.additional_images[2] ? 
+                                <img className="profile-board-images3" src={boards.additional_images[2]} /> 
+                                :
+                                <img className="profile-board-images3" src="https://res.cloudinary.com/djp7wsuit/image/upload/v1686021890/Untitled_design_1_xxxljj.png" />
+                            }
+                        </div>
                     </div>
                     <div className="profile-board-info"> 
                         <div>
