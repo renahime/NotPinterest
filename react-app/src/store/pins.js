7df75243ef9d98ba7c3c2386f902cd972266c99b
renahime <genuinehex@gmail.com>
2023-06-07 09:06:46 -0700

    pins

--- react-app/src/store/pins.js
+++ react-app/src/store/pins.js
@@ -56,25 +56,11 @@ export const getPinById = (pin_id) => async (dispatch) => {
     }
 
 }
-export const fetchPinsToday = () => async (dispatch) => {
-    const res = await fetch(`/api/pins/today`, {
-        method: 'GET'
-    })
-    if (res.ok) {
-        let pinsToday = await res.json()
-        dispatch(getPinsToday(pinsToday))
-    } else {
-        const errors = await res.json()
-        return errors
-    }
-}
 
 const initialState = { pins: {}, singlePin: {}, todayPins: {} }
 
 export default function pinsReducer(state = initialState, action) {
     switch (action.type) {
-        case GET_PINS_MADE_TODAY:
-            return { ...state, allBoards: { ...state.allPins }, todayPins: { ...action.pins } }
         case GET_PIN:
             return { ...state, pins: { ...state.pins }, singlePin: { ...action.pin } }
         case CREATE_PIN:
