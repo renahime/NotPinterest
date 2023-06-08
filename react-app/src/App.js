f454c9892d99fd5b88dede01074e6dd2eed870d1
Michael Golshani <michaelgolshani@gmail.com>
2023-06-08 11:10:26 -0700

    merge with cassandra working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -67,7 +67,7 @@ function App() {
             <UserPins />
           </Route>
           <Route path="/:username/_saved">
-            <ProfilePage re/>
+            <ProfilePage />
           </Route>
           <Route exact path="/:username/:boardName">
             <IndividualBoardPage />
