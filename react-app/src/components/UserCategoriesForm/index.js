027be588a00fe62cf979570ccc43068bceef5898
renahime <genuinehex@gmail.com>
2023-06-08 19:30:28 -0700

    changed class names

--- react-app/src/components/UserCategoriesForm/index.js
+++ react-app/src/components/UserCategoriesForm/index.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Redirect } from "react-router-dom";
 import { getPinsByCategory } from "../../store/pins";
 import "./UserCategoriesForm.css"
-import {  } from "react-router-dom/cjs/react-router-dom.min";
+import { } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function UserCategoriesForm() {
     const history = useHistory()
@@ -17,12 +17,12 @@ export default function UserCategoriesForm() {
     let [oldMoneyImage, setOldMneyImage] = useState(false)
     let [streetWearImage, setStreetwearImage] = useState(false)
 
-    let bohoImageClassName = bohoImage ? "selected" : "notSelected"
-    let athleisureImageClassName = athleisureImage ? "selected" : "notSelected"
-    let darkImageClassName = darkImage ? "selected" : "notSelected"
-    let formalImageClassName = formalImage ? "selected" : "notSelected"
-    let oldMoneyImageClassName = oldMoneyImage ? "selected" : "notSelected"
-    let streetwearImageClassName = streetWearImage ? "selected" : "notSelected"
+    let bohoImageClassName = bohoImage ? "selected-user" : "notSelected"
+    let athleisureImageClassName = athleisureImage ? "selected-user" : "notSelected"
+    let darkImageClassName = darkImage ? "selected-user" : "notSelected"
+    let formalImageClassName = formalImage ? "selected-user" : "notSelected"
+    let oldMoneyImageClassName = oldMoneyImage ? "selected-user" : "notSelected"
+    let streetwearImageClassName = streetWearImage ? "selected-user" : "notSelected"
 
     console.log("current user", currentUser)
     if (currentUser === null) history.push("/")
@@ -39,10 +39,10 @@ export default function UserCategoriesForm() {
         if (oldMoneyImage) interests.push("Old Money")
         if (athleisureImage) interests.push("Athleisure")
         if (!interests.length) {
-            return setErrors({errors: "Must select at least one category."})
+            return setErrors({ errors: "Must select at least one category." })
         } else {
-            let pins = await dispatch(getPinsByCategory(interests)).then((res) => <Redirect to="/feed" pins={res} /> )
-            if (pins.errors){
+            let pins = await dispatch(getPinsByCategory(interests)).then((res) => <Redirect to="/feed" pins={res} />)
+            if (pins.errors) {
                 return setErrors(pins.errors)
             }
         }
@@ -56,7 +56,7 @@ export default function UserCategoriesForm() {
                 <h3>It's like Pinterest, but for your clothes</h3>
                 <h4>Share all of your favorite trends with us</h4>
                 <h5>...but first, let us know what type of threads you favor so we can curate your feed!</h5>
-                {Object.values(errors).length ? <p style={{color:"red"}}>{errors.errors}</p> : null}
+                {Object.values(errors).length ? <p style={{ color: "red" }}>{errors.errors}</p> : null}
             </div>
             <div className="all-user-category-images-wrapper">
                 <div className="user-category-image-wrapper" onClick={() => setBohoImage(!bohoImage)}>
@@ -70,22 +70,22 @@ export default function UserCategoriesForm() {
                     {athleisureImage ? <i class={"fa-solid fa-check " + athleisureImageClassName}></i> : null}
                 </div>
                 <div className="user-category-image-wrapper" onClick={() => setDarkImage(!darkImage)}>
-                    <img  src="https://threadterest.s3.us-east-2.amazonaws.com/a30d5641ff84aee5a8af247d80f671bd.jpg" />
+                    <img src="https://threadterest.s3.us-east-2.amazonaws.com/a30d5641ff84aee5a8af247d80f671bd.jpg" />
                     <p>Dark</p>
                     {darkImage ? <i class={"fa-solid fa-check " + darkImageClassName}></i> : null}
                 </div>
                 <div className="user-category-image-wrapper" onClick={() => setFormalImage(!formalImage)}>
-                    <img  src="https://threadterest.s3.us-east-2.amazonaws.com/Tailored+Suit.jpeg" />
+                    <img src="https://threadterest.s3.us-east-2.amazonaws.com/Tailored+Suit.jpeg" />
                     <p>Formal</p>
                     {formalImage ? <i class={"fa-solid fa-check " + formalImageClassName}></i> : null}
                 </div>
                 <div className="user-category-image-wrapper" onClick={() => setOldMneyImage(!oldMoneyImage)}>
-                    <img  src="https://threadterest.s3.us-east-2.amazonaws.com/b57a07ed28ab9aef13b768873cb90704.jpg" />
+                    <img src="https://threadterest.s3.us-east-2.amazonaws.com/b57a07ed28ab9aef13b768873cb90704.jpg" />
                     <p>Old Money</p>
                     {oldMoneyImage ? <i class={"fa-solid fa-check " + oldMoneyImageClassName}></i> : null}
                 </div>
                 <div className="user-category-image-wrapper" onClick={() => setStreetwearImage(!streetWearImage)}>
-                    <img  src="https://threadterest.s3.us-east-2.amazonaws.com/69d784bab787d1a9bc1206b742b580af.jpg" />
+                    <img src="https://threadterest.s3.us-east-2.amazonaws.com/69d784bab787d1a9bc1206b742b580af.jpg" />
                     <p>Streetwear</p>
                     {streetWearImage ? <i class={"fa-solid fa-check " + streetwearImageClassName}></i> : null}
                 </div>
