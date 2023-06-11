546b4b788d851c0d22630272219e0b1b8d59b4a1
CaSandraSmith <casresm3@gmail.com>
2023-06-11 15:13:26 -0400

    Added mikey's feed to mine

--- react-app/src/components/FeedPage/casandra-feed.js
+++ react-app/src/components/FeedPage/casandra-feed.js
@@ -1,9 +1,12 @@
 import { useDispatch, useSelector } from "react-redux"
 import { getPinsByCategory } from "../../store/pins"
-import { useEffect, useState } from "react"
-import { useHistory } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import { useHistory, Link } from "react-router-dom"
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import { getAllPinsThunkOld } from "../../store/pins"
+import LoadingButton from "../LoadingButton";
+import OpenModalButton from '../OpenModalButton';
+import CreateBoardModal from "../CreateBoardModal";
 import "./casandra-feed.css"
 
 
@@ -14,11 +17,17 @@ export default function CaSandraFeed() {
     let [finishedLoading, setFinished] = useState(false)
     let pins = useSelector(state => state.pins.allPins)
     let sessionUser = useSelector(state => state.session.user)
+    const [hoverBoardDiv, setHoverBoardDiv] = useState("")
+    const [selectedBoardDropdown, setSelectedBoardDropdown] = useState(sessionUser.boards[0]?.name || "")
 
     let pinsArr = []
+    let numberOfPins = 0
+    let boards = 0
     if (pins && Object.values(pins) != null) {
-        pinsArr = filterPins(Object.values(pins))
-        console.log("pinsArr", pinsArr)
+        [pinsArr, numberOfPins] = filterPins(Object.values(pins))
+        if (sessionUser) {
+            boards = sessionUser.boards.length
+        }
     }
 
     function filterPins(pins) {
@@ -27,15 +36,17 @@ export default function CaSandraFeed() {
             return randomPins.splice(0, 30)
         } else {
             let filteredPins = []
+            let userPins = 0
             let categories = sessionUser.categories
             for (let pin of pins) {
+                if (pin.owner_id === sessionUser.id) userPins++
                 for (let category of categories) {
                     if (pin.categories.includes(category))
-                    filteredPins.push(pin)
+                        filteredPins.push(pin)
                 }
             }
             let randomPins = shufflePins(filteredPins)
-            return randomPins.splice(0, 30)
+            return [randomPins.splice(0, 30), userPins]
         }
     }
 
@@ -50,14 +61,51 @@ export default function CaSandraFeed() {
         return newPinOrder
     }
 
+    function viewIndividualBoard(username, name) {
+        let nameArr = name.toLowerCase().split(" ")
+        let formattedName = nameArr.join("_")
+        history.push(`/${username}/${formattedName}`)
+    }
+
+    const [boardColors] = useState([
+        { backgroundColor: "rgb(233, 212, 212)" },
+        { backgroundColor: "rgb(190, 205, 193)" },
+        { backgroundColor: "rgb(229, 235, 209)" },
+        { backgroundColor: "rgb(244, 230, 219)" },
+
+    ]);
+
+    const scrollContainerRef = React.useRef(null);
+
+    const handleScrollLeft = () => {
+        const scrollContainer = scrollContainerRef.current;
+        scrollContainer.scrollTo({
+            left: scrollContainer.scrollLeft - 200,
+            behavior: 'smooth' // You can remove this line if you prefer an instant scroll
+        });
+    };
+
+    const handleScrollRight = () => {
+        const scrollContainer = scrollContainerRef.current;
+        scrollContainer.scrollTo({
+            left: scrollContainer.scrollLeft + 200,
+            behavior: 'smooth' // You can remove this line if you prefer an instant scroll
+        });
+    };
+
+    let formatBoards = (n) => {
+        if (n === 1) {
+            return "1 board"
+        } else {
+            return `${n} boards`
+        }
+    }
 
     useEffect(() => {
         dispatch(getAllPinsThunkOld()).then(() => setLoading(true))
     }, [dispatch])
 
     useEffect(() => {
-        console.log(loading, "loadingloadingloading")
-        console.log("pins", pins)
         if (!loading && !pins) {
             return
         }
@@ -71,31 +119,94 @@ export default function CaSandraFeed() {
         }
     }, [loading, pins])
 
-    if (!pinsArr[0]?.id || !finishedLoading) return <h1>...Loading</h1>
-    
+    if (!pinsArr[0]?.id || !finishedLoading) {
+        return (
+            <LoadingButton
+                isLoading={loading}
+            // disabled={isLoading}
+            />
+        )
+    }
+
     return (
-        <div className="pins-feed-wrapper-wrapper">
-            <ResponsiveMasonry className="pins-feed-wrapper" options={{fitWidth: true}}
-                columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4, 1200: 5, 1900: 6 }}>
-                <Masonry className="feed-pin-masonry" options={{fitWidth: true}}>
-                    {pinsArr.map(pin => (
-                        <div className="feed-individual-pin-wrapper">
-                            <div onClick={() => history.push(`/pin/${pin.id}`)}>
-                                <img className="feed-pin-image" src={pin.image} alt={pin.alt_text ? pin.alt_text : ""} />
-                            </div>
-                            <div className="feed-individual-pin-info" onClick={() => history.push(`/${pin.user.username}`)}>
-                                <div>
-                                    {pin.title ? <p className="feed-pin-title">{pin.title}</p> : null}
-                                </div>
-                                <div className="feed-individual-pin-user-info">
-                                {pin.user.profile_image ? <img className="feed-profile-image" src={pin.user.profile_image} /> : null}
-                                <p>{pin.user.username}</p>
-                                </div>
+        <div>
+            {Object.values(sessionUser).length === 0 ?
+                <h3>Sign up for Threadterest today and make some new threads!</h3> :
+                boards > 0 ?
+                    <div>
+                        <div>
+                            <div className="board-container-top-text">
+                                <h3>
+                                    Hey {sessionUser.first_name}, you have <span><Link to={`/${sessionUser.username}`}> {formatBoards(boards)}</Link> </span> and <span><Link to={`/${sessionUser.username}/_created`}> {numberOfPins} pins.</Link></span
+                                    > Check them out!
+                                </h3>
                             </div>
                         </div>
-                    ))}
-                </Masonry>
-            </ResponsiveMasonry>
-        </div>
+                        <div className="full-board-container">
+                            <div className="board-container" ref={scrollContainerRef}>
+                                <div className="scroll-arrows left-arrow" onClick={handleScrollLeft}>
+                                    <i className="fa-solid fa-angle-left"></i>
+                                </div>
+                                <div className="scroll-arrows right-arrow" onClick={handleScrollRight}>
+                                    <i className="fa-solid fa-angle-left fa-rotate-180"></i>
+                                </div>
+                                {sessionUser.boards.map((board, index) => (
+                                    <div key={board.id} className="board-top" style={boardColors[index % boardColors.length]} onClick={() => viewIndividualBoard(sessionUser.username, board.name)}
+                                    >
+                                        <div>
+                                            {board.name}
+                                        </div>
+                                    </div>
+                                ))}
+
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="boardSelect">Select a board:</label>
+                            <select id="boardSelect" onChange={(e) => setSelectedBoardDropdown(e.target.value)} value={selectedBoardDropdown}>
+                                {sessionUser.boards.map((board) => (
+                                    <option key={board.id} value={board.name}>
+                                        {board.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                    :
+                    <div className="board-container-top-text">
+                        <h3>
+                            Oh no, you have <span><Link to={`/${sessionUser.username}`}> {boards.length} boards.</Link></span> Let's change that!
+                            <OpenModalButton
+                                buttonText="Create Board"
+                                className="feed-page-create-board"
+                                modalComponent={<CreateBoardModal username={sessionUser?.username} />}
+                            />
+                        </h3>
+                    </div>
+                    }
+            < div className="pins-feed-wrapper-wrapper">
+                <ResponsiveMasonry className="pins-feed-wrapper" options={{ fitWidth: true }}
+                    columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4, 1200: 5, 1900: 6 }}>
+                    <Masonry className="feed-pin-masonry" options={{ fitWidth: true }}>
+                        {pinsArr.map(pin => (
+                            <div className="feed-individual-pin-wrapper">
+                                <div onClick={() => history.push(`/pin/${pin.id}`)}>
+                                    <img className="feed-pin-image" src={pin.image} alt={pin.alt_text ? pin.alt_text : ""} />
+                                </div>
+                                <div className="feed-individual-pin-info" onClick={() => history.push(`/${pin.user.username}`)}>
+                                    <div>
+                                        {pin.title ? <p className="feed-pin-title">{pin.title}</p> : null}
+                                    </div>
+                                    <div className="feed-individual-pin-user-info">
+                                        {pin.user.profile_image ? <img className="feed-profile-image" src={pin.user.profile_image} /> : null}
+                                        <p>{pin.user.username}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </div >
     )
 }
--- react-app/src/components/FeedPage/index.js
+++ react-app/src/components/FeedPage/index.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Redirect, useHistory } from "react-router-dom";
 // import { getAllBoardThunks } from "../../store/boards-mikey";
-// import { getBoardsByUsername } from "../../store/boards";
+import { getBoardsByUsername } from "../../store/boards";
 import LoadingButton from "../LoadingButton";
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
 import CreateBoardModal from "../CreateBoardModal";
 import UpdateBoardModal from "../UpdateBoardModal";
+import { getAllPinsThunkOld } from "../../store/pins";
 import './FeedPage.css'
 
 
@@ -20,19 +21,35 @@ function FeedPage() {
   const [selectedBoardId, setSelectedBoardId] = useState(null);
   const [hoverBoard, setHoverBoard] = useState(false)
   const [hoverBoardDiv, setHoverBoardDiv] = useState("")
+  const [randomizedArrays, setRandomizedArrays] = useState([]);
+
+  // let singleBoard = useSelector(state => state.boards.singleBoard)
+  let state = useSelector(state => state)
+  let session = useSelector(state => state.session)
+  console.log("SESSION", session)
+  // console.log("SINGLE BOARD INFO on feed page", singleBoard)
+  console.log("CURRENT STATE", state)
+  console.log("BOARDS", state.boards)
+
+
 
   console.log("HOVER PIN", hoverBoardDiv)
 
+
+
+  //for BOARD HOVER
   function onHoverBoard(board) {
     setHoverBoard(true)
     setHoverBoardDiv(board.id)
   }
-
   function offHoverBoard() {
     setHoverBoard(false)
     setHoverBoardDiv("")
   }
 
+
+
+  // if user clicks on board
   function viewIndividualBoard(username, name) {
     let nameArr = name.toLowerCase().split(" ")
     let formattedName = nameArr.join("_")
@@ -40,41 +57,94 @@ function FeedPage() {
   }
 
 
+
+  //FETCH BOARD DATA
   // useEffect(() => {
   //   setTimeout(() => {
-  //     // Assuming you're fetching the boards data here
+
   //     if (username) {
   //       dispatch(getBoardsByUsername(username))
   //     }
-
   //     setIsLoading(false);
   //   }, 1500);
-
   // }, [dispatch])
 
 
+let allPinsArr;
+
+  //GET ALL PINS DATA FOR ARRAY
+
+  const allPins = useSelector(state => state.pins.allPins)
+  console.log("WE ARE TRYING TO GET PINS" , allPins)
+  if(allPins){
+    console.log("WE ARE IN ALL PINS", allPins)
+    allPinsArr = Object.values(allPins)
+    console.log("ALL PINS ARRAY", allPinsArr)
+  }else {
+    allPinsArr = []
+  }
+
+
+
+  useEffect(() => {
+    console.log("Before dispatch");
+    dispatch(getAllPinsThunkOld())
+      .then(() => console.log("ALL PINS FETCHED", allPinsArr))
+      .catch((error) => console.log("Error fetching pins:", error));
+  }, [dispatch]);
 
 
 
 
   //GET STATE DATA
   const sessionUser = useSelector(state => state.session.user)
-
   const currentState = useSelector(state => state)
-
-
   const username = sessionUser?.username
+  const boards = useSelector((state) => state.session.user.boards)
+  // const boards = Object.values(boardsSelector)
+  const [selectedBoard, setSelectedBoard] = useState(boards[0]?.id || "")
+  const [selectedBoardDropdown, setSelectedBoardDropdown] = useState(boards[0]?.name || "")
+
+  console.log("SELECETED BOARD DROPDOWN", selectedBoardDropdown)
   console.log("SESSION USER USERNAME", username)
-
   console.log(sessionUser)
-
-  const boardsSelector = useSelector((state) => state.boards.currentProfileBoards)
-  console.log("GET ALL BOARDS STATE TEST", boardsSelector)
-
-
-  const boards = Object.values(boardsSelector)
-  // const boards = boardsArr[0]
+  // console.log("GET ALL BOARDS STATE TEST", boardsSelector)
   console.log("GET ALL BOARDS DATA", boards)
+
+
+
+  // Randomize Array function
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  // let randomizedArrays;
+  const dividedArrays = [];
+
+
+  //FOR PIN FEED, sepereate all pins into 7 sections
+  useEffect(() => {
+    if (allPinsArr.length > 0) {
+      const dividedBy7 = Math.ceil(allPinsArr.length / 7);
+      for (let i = 0; i < allPinsArr.length; i += dividedBy7) {
+        const individualSection = allPinsArr.slice(i, i + dividedBy7);
+        dividedArrays.push(individualSection);
+      }
+      if (dividedArrays.length > 0) {
+        setRandomizedArrays(shuffleArray(dividedArrays));
+      }
+      console.log("DIVIDED ARRAYS", dividedArrays);
+      console.log("Randomized arrays", randomizedArrays)
+    }
+
+  }, [allPinsArr.length]);
+
+
+
+
 
 
 
@@ -82,11 +152,12 @@ function FeedPage() {
   // Checking for the number of pins in BOARD STATE
   let numberOfPins;
 
-  if (boards) {
+  if (boards.length > 0) {
     let numberOfPinsStart = 0
     for (let i = 0; i < boards.length; i++) {
       const board = boards[i]
-      if (board.pins.length > 0) {
+      console.log("BOARDS PINS LENGTH", board)
+      if (board.pins?.length > 0) {
         numberOfPinsStart += board.pins.length
         console.log("NUMBER OF PINS", numberOfPins)
       }
@@ -95,7 +166,9 @@ function FeedPage() {
   }
 
 
-
+// function addPinToBoard{
+//   await dispatch(pinThunk(singlePin, boardId));
+// }
 
   // our boards container will loop through this array and keep assigning board backgrounds dynamically
 
@@ -128,8 +201,17 @@ function FeedPage() {
   };
 
 
+
+  // this is for the select a board dropdown
+  const handleBoardChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedBoardDropdown(selectedValue);
+  };
+
+
+
   //check if we have any boards in our database or state. If not, we will load the page or not render it at all
-  if (typeof boards === 'undefined' || boards.length === 0) {
+  if (typeof boards === 'undefined' || boards.length === 0 || allPinsArr.length == 0 || dividedArrays.length == 0) {
 
     //Loading screen
     if (isLoading) {
@@ -168,9 +250,10 @@ function FeedPage() {
                   <i className="fa-solid fa-angle-left fa-rotate-180"></i>
                 </div>
 
+
                 {boards.map((board, index) => (
 
-                  <div key={board.id} className="board-top" style={boardColors[index % boardColors.length]} onClick={() => viewIndividualBoard(board.user.username, board.name)} onMouseEnter={() => onHoverBoard(board)} onMouseLeave={() => offHoverBoard()}>
+                  <div key={board.id} className="board-top" style={boardColors[index % boardColors.length]} onClick={() => viewIndividualBoard(session.user.username, board.name)} onMouseEnter={() => onHoverBoard(board)} onMouseLeave={() => offHoverBoard()}>
                     {/* <OpenModalButton
                   buttonText={board.name}
                   className="test-open-create-board-modal"
@@ -184,6 +267,18 @@ function FeedPage() {
                 ))}
 
               </div>
+            </div>
+
+
+            <div>
+              <label htmlFor="boardSelect">Select a board:</label>
+              <select id="boardSelect" onChange={handleBoardChange} value={selectedBoardDropdown}>
+                {boards.map((board) => (
+                  <option key={board.id} value={board.name}>
+                    {board.name}
+                  </option>
+                ))}
+              </select>
             </div>
           </>
 
@@ -219,201 +314,27 @@ function FeedPage() {
 
 
 
-
         <div className="feed-test-container">
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-              <button className="gallery-photo-button">Save</button>
-              <NavLink to="https://www.google.com" target="_blank" className="feed-test-link">
-                google.com
-              </NavLink>
-              <p className="feed-test-board-link">
-                Street Wear <span className="arrow-down">&lt;</span>
-              </p>
+          {randomizedArrays.map((array, index) => (
+            <div key={index} style={{ flex: 1 }}>
+              {array.map((item, itemIndex) => (
+                <div key={itemIndex} className="feed-test-photo">
+                  <img src={item.image} />
+                  <button className="gallery-photo-button" >Save</button>
+                  <NavLink to="/" target="_blank" className="feed-test-link">
+                    Test Link
+                  </NavLink>
+                  <p className="feed-test-board-link">
+                    {item.boardName} <span className="arrow-down">&lt;</span>
+                  </p>
+                </div>
+              ))}
             </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/474x/60/47/d2/6047d21562d5987476b46c752577b714.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/e1/6f/46/e16f46b30163cae38d8be85dcf2c0b0f.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-          </div>
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/d2/a9/1d/d2a91dac35bebb61531dad0d10e2d453.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/02/6a/e5/026ae51de6667442616920e2f1922467.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/d2/a9/1d/d2a91dac35bebb61531dad0d10e2d453.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/474x/60/47/d2/6047d21562d5987476b46c752577b714.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-
-          </div>
-
-
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/6f/a6/e1/6fa6e1cf1d653f0af05500966670b19e.jpg" />
-              <button className="gallery-photo-button">Save</button>
-
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/e1/6f/46/e16f46b30163cae38d8be85dcf2c0b0f.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/a0/10/99/a0109904e0c64c9349af920824161fc0.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/85/00/f6/8500f61d2686c9bc7310c39034c66437.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://cdn.shopify.com/s/files/1/0743/1575/9923/products/b4f15ed3ee7747f39b133a66ba3e2bff-Max_1080x.jpg?v=1683763998" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-
-          </div>
-
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/474x/e6/44/09/e644093a7ddb208e51b1a34d6dca1839.jpg" />
-              <button className="gallery-photo-button">Save</button>
-
-            </div>
-
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/d2/eb/2e/d2eb2ee1a96270af89570fb7e3ac3f3f.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/e1/6f/46/e16f46b30163cae38d8be85dcf2c0b0f.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/474x/e6/44/09/e644093a7ddb208e51b1a34d6dca1839.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/d2/eb/2e/d2eb2ee1a96270af89570fb7e3ac3f3f.jpg" />
-              <button className="gallery-photo-button">Save</button>
-            </div>
-
-          </div>
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/d2/a9/1d/d2a91dac35bebb61531dad0d10e2d453.jpg" />
-            </div>
-
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/02/6a/e5/026ae51de6667442616920e2f1922467.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/02/6a/e5/026ae51de6667442616920e2f1922467.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/d2/eb/2e/d2eb2ee1a96270af89570fb7e3ac3f3f.jpg" />
-            </div>
-          </div>
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/6f/a6/e1/6fa6e1cf1d653f0af05500966670b19e.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/e1/6f/46/e16f46b30163cae38d8be85dcf2c0b0f.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/d2/eb/2e/d2eb2ee1a96270af89570fb7e3ac3f3f.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-            </div>
-
-          </div>
-
-
-          <div style={{ flex: 1 }}>
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/474x/e6/44/09/e644093a7ddb208e51b1a34d6dca1839.jpg" />
-            </div>
-
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/d2/eb/2e/d2eb2ee1a96270af89570fb7e3ac3f3f.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-            </div>
-
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/564x/95/37/a9/9537a91068b3e44e5a9ee4b912b06882.jpg" />
-            </div>
-
-            <div className="feed-test-photo">
-              <img src="https://i.pinimg.com/736x/e1/6f/46/e16f46b30163cae38d8be85dcf2c0b0f.jpg" />
-            </div>
-
-          </div>
-
-
+          ))}
         </div>
+
+
+
 
 
 
