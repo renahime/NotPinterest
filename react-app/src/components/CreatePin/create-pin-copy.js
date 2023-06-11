7bd6589a03ced18e49a3a915f17cb2fef2e83250
CaSandraSmith <casresm3@gmail.com>
2023-06-11 19:42:23 -0400

    Removed feed and create-pin-pages.

---
+++ react-app/src/components/CreatePin/create-pin-copy.js
@@ -0,0 +1,296 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewPin } from "../../store/pins";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { getAllBoardsThunk } from "../../store/boards";
+import { useModal } from "../../context/Modal";
+import CreateNewBoardOnPin from "./CreateNewBoardOnPin";
+import "./CreatePin.css"
+
+
+export default function CreatePin() {
+    const history = useHistory()
+    const currentUser = useSelector(state => state.session.user)
+    const currentUserBoards = useSelector(state => state.session.user.boards)
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [errors, setErrors] = useState("")
+    const [viewAltText, setViewAltText] = useState(false)
+    const [altText, setAltText] = useState("")
+    const [destinationLink, setDestinationLink] = useState("")
+    const [loadingImage, setLoadingImage] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
+    const [board, setBoard] = useState("")
+    const [titleActive, setTitleActive] = useState(false)
+    const [descActive, setDescActive] = useState(false)
+    const [altActive, setAltActive] = useState(false)
+    const [image, setImage] = useState("")
+    const [previewImage, setPreviewImage] = useState("")
+    const [submit, setSubmit] = useState(false)
+    const { setModalContent, setOnModalClose } = useModal();
+    const dispatch = useDispatch()
+    let userBoards = Object.values(currentUser.boards)
+
+    function formatFollowers(num) {
+        if (num === 1) return "1 follower"
+        else return `${num} followers`
+    }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(board)
+        setErrors({})
+
+        let validationErrors = {}
+
+        if (!image) {
+            validationErrors["image"] = "Image is required"
+        }
+        if (title.length > 100) {
+            validationErrors.title = "Ooops! This title is getting long. Try trimming it down."
+        }
+        if (description.length > 225) {
+            validationErrors.description = "Ooops! This description is getting long. Try trimming it down."
+        }
+        if (altText.length > 225) {
+            validationErrors.altText = "Ooops! This alt text is getting long. Try trimming it down."
+        }
+        if (destinationLink.length > 225) {
+            validationErrors.destinationLink = "Ooops! This link is getting long. Try trimming it down."
+        }
+        if (!board) {
+            validationErrors.board = "Ooops! This pin must be added to a board!"
+        }
+        if (Object.values(validationErrors).length) {
+            setErrors(validationErrors)
+            return
+        }
+        setSubmit(true)
+        setImage(previewImage)
+
+        const pinData = new FormData()
+        pinData.append("image", image)
+        pinData.append("board", board)
+        pinData.append("title", title)
+        pinData.append("description", description)
+        pinData.append("alt_text", altText)
+        pinData.append("destination", destinationLink)
+        setLoadingImage(true)
+
+        let new_pin = await dispatch(createNewPin(pinData)).then(setLoadingImage(false))
+        if (new_pin.errors) {
+            setErrors(new_pin.errors)
+            return
+        } else {
+            history.push(`/pin/${new_pin.pin.id}`)
+        }
+
+    }
+
+    function openModal (e) {
+        e.preventDefault()
+        setModalContent(<CreateNewBoardOnPin />)
+    }
+
+    let altTextButtonClassName = viewAltText ? "hidden" : "create-pin-alt-text-button"
+    let altTextInputClassName = viewAltText ? "create-pin-alt-text-input" : "hidden"
+    let pinMenuClassName = showMenu ? "new-pin-menu" : "hidden"
+    let activeTitleClassName = titleActive ? "" : "hidden"
+    let activeDescClassName = descActive ? "" : "hidden"
+    let activeAltClassName = altActive ? "" : "hidden"
+
+    useEffect(() => {
+        if (submit) {
+            return
+        }
+        if (image) {
+            let prev = URL.createObjectURL(image)
+            setPreviewImage(prev)
+            return () => {
+                URL.revokeObjectURL(prev)
+            }
+        }
+    }, [image])
+
+    useEffect(() => {
+        if (userBoards.length && !board) {
+            setBoard(userBoards[0].id)
+            console.log(userBoards)
+        }
+    }, [userBoards])
+
+
+    return (
+        <div className="new-pin-wrapper">
+            <form onSubmit={e => handleSubmit(e)} encType="multipart/form-data" className="new-pin-form">
+                <div className="new-pin-image-side">
+                    <i className="fa-solid fa-ellipsis new-pin-open-menu" onClick={() => setShowMenu(!showMenu)}></i>
+                    <div className={pinMenuClassName}>
+                        <p onClick={() => history.push("/feed")}>Delete</p>
+                    </div>
+                    <div className="file-input-wrapper-wrapper">
+                        <div className="file-input-wrapper">
+                            <label className="new-pin-image">
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={(e) => setImage(e.target.files[0])}
+                                    required
+                                />
+                                <div className="new-pin-image-file-styled">
+                                    {previewImage ? <img className="new-pin-preview-image" src={previewImage} /> : <div className="new-pin-default-image">
+                                        <div className="new-pin-upload-caption">
+                                            <i className="fa-solid fa-circle-arrow-up"></i>
+                                            <p>Drag and drop or click to upload</p>
+                                        </div>
+                                        <p>We recommend using high quality .jpg files less than 20MB</p>
+                                    </div>}
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                    {errors.image ? <p className="create-pin-errors">{errors.image}</p> : null}
+                    {loadingImage ? <h3>Wait while your image is uploaded</h3> : null}
+                </div>
+                <div className="new-pin-info-side">
+                    {currentUserBoards.length ?
+                        <div className="new-pin-save-and-board">
+                            <label>
+                                <select
+                                    value={board}
+                                    onChange={(e) => setBoard(e.target.value)}
+                                >
+                                    {currentUserBoards.map(boardValue => (
+                                        <option
+                                            key={boardValue.id}>
+                                            {boardValue.name}
+                                        </option>
+                                    ))}
+                                </select>
+                                {errors.board ? <p className="create-pin-errors">{errors.board}</p> : null}
+                            </label>
+                            <button>Save</button>
+                        </div>
+                        :
+                        <button onClick={openModal} className='open-modal-button'>
+                            Create a Board
+                            <i className="fa-solid fa-plus"></i>
+                        </button>
+                    }
+                    <div className="new-pin-title-input-wraper">
+                        <label>
+                            <input
+                                onFocus={() => setTitleActive(true)}
+                                onBlur={() => setTitleActive(false)}
+                                className="new-pin-input new-pin-title"
+                                type="text"
+                                value={title}
+                                onChange={e => setTitle(e.target.value)}
+                                placeholder="Add your title"
+                            // maxLength={100}
+                            />
+                            {errors.title || title.length > 100 ?
+                                <div className="new-pin-active-caption">
+                                    <p className={`create-pin-errors`}>
+                                        {errors.title ? errors.title : " Ooops! This title is getting long. Try trimming it down."
+                                        }
+                                    </p>
+                                    <p className="create-pin-errors">{100 - title.length}</p>
+                                </div>
+                                :
+                                <div className={`new-pin-active-caption ${activeTitleClassName}`}>
+                                    <p>Your first 40 characters are what usually show up in feeds</p>
+                                    <p>{100 - title.length}</p>
+                                </div>
+                            }
+                        </label>
+                    </div>
+                    <div className="create-pin-owner-followers-image">
+                        <div>
+                            {currentUser.profile_image ? <img className="create-pin-profile-image" src={currentUser.profile_image} /> : <i className=" create-pin-default-image fa-solid fa-circle-user"></i>}
+                        </div>
+                        <div className="create-pin-owner-name-followers">
+                            <p className="create-pin-owner">{currentUser.first_name} {currentUser.last_name}</p>
+                            <p>{formatFollowers(currentUser?.followers.length)}</p>
+                        </div>
+                    </div>
+                    <div className="new-pin-description-input-wrapper">
+                        <label>
+                            <input
+                                onFocus={() => setDescActive(true)}
+                                onBlur={() => setDescActive(false)}
+                                className="new-pin-input new-pin-description"
+                                type="text"
+                                value={description}
+                                onChange={e => setDescription(e.target.value)}
+                                placeholder="Tell everyone what your Thread is about"
+                            // maxLength={225}
+                            />
+                            {errors.description || description.length > 225 ?
+                                <div className="new-pin-active-caption">
+                                    <p className="create-pin-errors ">
+                                        {errors.description ? errors.description : " Ooops! This description is getting long. Try trimming it down."
+                                        }
+                                    </p>
+                                    <p className="create-pin-errors">{225 - description.length}</p>
+                                </div>
+                                :
+                                <div className={`new-pin-active-caption ${activeDescClassName}`}>
+                                    <p>People will usually see the first 50 characters when they click on your pin</p>
+                                    <p>{225 - description.length}</p>
+                                </div>
+                            }
+
+                        </label>
+                    </div>
+                    <div className="new-pin-alt-text-input-wraper">
+                        <p onClick={() => setViewAltText(true)} className={altTextButtonClassName}>Add alt text</p>
+                        <div className="new-pin-alt-and-destination-wraper-with-button">
+                            <label>
+                                <input
+                                    onFocus={() => setAltActive(true)}
+                                    onBlur={() => setAltActive(false)}
+                                    className={`new-pin-input ${altTextInputClassName}`}
+                                    type="text"
+                                    value={altText}
+                                    onChange={e => setAltText(e.target.value)}
+                                    placeholder="Explain what people can see in your Thread"
+                                // maxLength={225}
+                                />
+                                {errors.altText || altText.length > 225 ?
+                                    <div className="new-pin-active-caption">
+                                        <p className="create-pin-errors ">
+                                            {errors.altText ? errors.altText : " Ooops! This alternate text is getting long. Try trimming it down."
+                                            }
+                                        </p>
+                                        <p className="create-pin-errors">{225 - altText.length}</p>
+                                    </div>
+                                    :
+                                    <div className={`new-pin-active-caption ${activeAltClassName}`}>
+                                        <p>This text will be read aloud by screen readers</p>
+                                        <p>{225 - altText.length}</p>
+                                    </div>
+                                }
+                            </label>
+                        </div>
+                    </div>
+                    <div className="new-pin-alt-and-destination-wraper-with-button">
+                        <label>
+                            <input
+                                className="new-pin-input"
+                                type="url"
+                                value={destinationLink}
+                                onChange={e => setDestinationLink(e.target.value)}
+                                placeholder="Add a destination link"
+                            // maxLength={225}
+                            />
+                            {errors.destinationLink ? <p className="create-pin-errors">{errors.destinationLink}</p> : null}
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
---
+++ react-app/src/components/FeedPage/casandra-feed-copy.js
@@ -0,0 +1,238 @@
+import { useDispatch, useSelector } from "react-redux"
+import { getPinsByCategory } from "../../store/pins"
+import React, { useEffect, useState } from "react"
+import { useHistory, NavLink } from "react-router-dom"
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import { getAllPinsThunkOld } from "../../store/pins"
+import LoadingButton from "../LoadingButton";
+import OpenModalButton from '../OpenModalButton';
+import CreateBoardModal from "../CreateBoardModal";
+import "./casandra-feed.css"
+
+
+export default function CaSandraFeed() {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    let [loading, setLoading] = useState(false)
+    let [finishedLoading, setFinished] = useState(false)
+    let pins = useSelector(state => state.pins.allPins)
+    let sessionUser = useSelector(state => state.session.user)
+    
+    const [hoverBoard, setHoverBoard] = useState(false)
+    const [hoverBoardDiv, setHoverBoardDiv] = useState("")
+    const [selectedBoardDropdown, setSelectedBoardDropdown] = useState(sessionUser?.boards[0]?.name || "")
+    const [currentUser, setCurrentUser] = useState(sessionUser ? true : false)
+
+    let pinsArr = []
+    let numberOfPins = 0
+    let boards = 0
+    if (loading && Object.values(pins).length) {
+        const { filteredPinsArr, userPins } = filterPins(Object.values(pins))
+        pinsArr = filteredPinsArr
+        if (sessionUser) {
+            numberOfPins = userPins
+            boards = sessionUser.boards.length
+        }
+    }
+
+    function filterPins(pins) {
+        if (!sessionUser || sessionUser.categories.length === 0) {
+            console.log(pins)
+            let randomPins = shufflePins(pins)
+            console.log(randomPins.splice(0, 30))
+            return {
+                filteredPinsArr: randomPins.splice(0, 30),
+                userPins: null
+            }
+        } else {
+            let filteredPins = []
+            let userPins = 0
+            let categories = sessionUser.categories
+            for (let pin of pins) {
+                if (pin.owner_id === sessionUser.id) userPins++
+                for (let category of categories) {
+                    if (pin.categories.includes(category))
+                        filteredPins.push(pin)
+                }
+            }
+            let randomPins = shufflePins(filteredPins)
+            console.log("random pins: ", randomPins)
+            return {
+                filteredPinsArr: randomPins.splice(0, 30),
+                userPins: userPins
+            }
+        }
+    }
+
+    function shufflePins(pins) {
+        let newPinOrder = []
+        let copy = [...pins]
+        while (copy.length) {
+            let i = Math.floor(Math.random() * copy.length)
+            newPinOrder.push(copy[i])
+            copy.splice(i, 1)
+        }
+        return newPinOrder
+    }
+
+    function viewIndividualBoard(username, name) {
+        let nameArr = name.toLowerCase().split(" ")
+        let formattedName = nameArr.join("_")
+        history.push(`/${username}/${formattedName}`)
+    }
+
+    const [boardColors] = useState([
+        { backgroundColor: "rgb(233, 212, 212)" },
+        { backgroundColor: "rgb(190, 205, 193)" },
+        { backgroundColor: "rgb(229, 235, 209)" },
+        { backgroundColor: "rgb(244, 230, 219)" },
+
+    ]);
+
+    const scrollContainerRef = React.useRef(null);
+
+    const handleScrollLeft = () => {
+        const scrollContainer = scrollContainerRef.current;
+        scrollContainer.scrollTo({
+            left: scrollContainer.scrollLeft - 200,
+            behavior: 'smooth' // You can remove this line if you prefer an instant scroll
+        });
+    };
+
+    const handleScrollRight = () => {
+        const scrollContainer = scrollContainerRef.current;
+        scrollContainer.scrollTo({
+            left: scrollContainer.scrollLeft + 200,
+            behavior: 'smooth' // You can remove this line if you prefer an instant scroll
+        });
+    };
+
+    function onHoverBoard(board) {
+        setHoverBoard(true)
+        setHoverBoardDiv(board.id)
+    }
+    function offHoverBoard() {
+        setHoverBoard(false)
+        setHoverBoardDiv("")
+    }
+    let formatBoards = (n) => {
+        if (n === 1) {
+            return "1 board"
+        } else {
+            return `${n} boards`
+        }
+    }
+
+    useEffect(() => {
+        dispatch(getAllPinsThunkOld()).then(() => setLoading(true))
+    }, [dispatch])
+
+    useEffect(() => {
+        if (!loading && !pins) {
+            return
+        }
+        if (!pins || !Object.values(pins).length) {
+            return
+        }
+        else {
+            console.log("pins in use", pins)
+            setFinished(true)
+        }
+    }, [loading, pins, currentUser])
+    console.log("pinsArr", pinsArr)
+    if (!Object.values(pins).length) {
+        return (
+            <LoadingButton
+                isLoading={loading}
+            // disabled={isLoading}
+            />
+        )
+    }
+    console.log("session user", sessionUser)
+    return (
+        <div>
+            {(sessionUser && boards.length) ? (
+
+                <>
+                    <div className="board-container-top-text">
+                        <div>Hey {sessionUser.first_name}, you have</div>
+                        <NavLink to={`/${sessionUser.username}`}> {sessionUser.boards.length} boards</NavLink>
+                        <div>and</div>
+                        <NavLink to={`/${sessionUser.username}/_created`}>{numberOfPins} pins</NavLink>
+                        <div>Check them out!</div>
+                    </div>
+
+                    <div className="full-board-container">
+
+                        <div className="board-container" ref={scrollContainerRef}>
+                            <div className="scroll-arrows left-arrow" onClick={handleScrollLeft}>
+                                <i className="fa-solid fa-angle-left"></i>
+                            </div>
+                            <div className="scroll-arrows right-arrow" onClick={handleScrollRight}>
+                                <i className="fa-solid fa-angle-left fa-rotate-180"></i>
+                            </div>
+
+                            {boards.map((board, index) => (
+
+                                <div key={board.id} className="board-top" style={boardColors[index % boardColors.length]} onClick={() => viewIndividualBoard(board.user.username, board.name)} onMouseEnter={() => onHoverBoard(board)} onMouseLeave={() => offHoverBoard()}>
+                                    {/* <OpenModalButton
+                            buttonText={board.name}
+                            className="test-open-create-board-modal"
+                            modalComponent={<UpdateBoardModal id={board.id} />}
+                            onClick={() => history.push(`/boards/${board.id}`)}
+                          /> */}
+                                    <div>
+                                        {board.name}
+                                    </div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+                </>
+
+            ) : ((sessionUser && !boards.length) ? (
+                <>
+                    <div className="board-container-top-text">
+                        <div>Oh no, you have</div>
+                        <NavLink to={`/${sessionUser.username}`}> {sessionUser.boards.length} boards.</NavLink>
+                        <div>Let's change that!</div>
+                        <OpenModalButton
+                            buttonText="Create Board"
+                            className="feed-page-create-board"
+                            modalComponent={<CreateBoardModal username={sessionUser?.username} />}
+                        />
+                    </div>
+                </>
+            ) : (null)
+            )
+            }
+            < div className="pins-feed-wrapper-wrapper">
+                <ResponsiveMasonry className="pins-feed-wrapper" options={{ fitWidth: true }}
+                    columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4, 1200: 5, 1900: 6 }}>
+                    <Masonry className="feed-pin-masonry" options={{ fitWidth: true }}>
+                        {pinsArr.map(pin => (
+                            <div
+                                // onHover={() =>}
+                                className="feed-individual-pin-wrapper">
+                                <button className="feed-save-button">Save</button>
+                                <div onClick={() => history.push(`/pin/${pin.id}`)}>
+                                    <img className="feed-pin-image" src={pin.image} alt={pin.alt_text ? pin.alt_text : ""} />
+                                </div>
+                                <div className="feed-individual-pin-info" onClick={() => history.push(`/${pin.user.username}`)}>
+                                    <div>
+                                        {pin.title ? <p className="feed-pin-title">{pin.title}</p> : null}
+                                    </div>
+                                    <div className="feed-individual-pin-user-info">
+                                        {pin.user.profile_image ? <img className="feed-profile-image" src={pin.user.profile_image} /> : null}
+                                        <p>{pin.user.username}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </div >
+    )
+}
